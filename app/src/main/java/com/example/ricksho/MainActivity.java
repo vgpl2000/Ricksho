dcad7c24b0518ace5d65816bfaa8aff73eb24421
vgpl2000 <venugpl2000@gmail.com>
2023-04-09 03:19:46 -0700

    home page progress

--- app/src/main/java/com/example/ricksho/MainActivity.java
+++ app/src/main/java/com/example/ricksho/MainActivity.java
@@ -3,16 +3,20 @@ package com.example.ricksho;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_hire;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_hire=findViewById(R.id.btn_hire);
+        
     }
 }
