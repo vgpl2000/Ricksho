ea7e0cde7ce8ab0213b67dd7abbf08143e79649b
vgpl2000 <venugpl2000@gmail.com>
2023-04-09 04:38:56 -0700

    home page progress

--- app/src/main/java/com/example/ricksho/MainActivity.java
+++ app/src/main/java/com/example/ricksho/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.ricksho;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -11,12 +16,21 @@ import com.airbnb.lottie.LottieAnimationView;
 public class MainActivity extends AppCompatActivity {
     Button btn_hire;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //To change color of status bar
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(ContextCompat.getColor(this, R.color.yellow));
+        }
+
+
         btn_hire=findViewById(R.id.btn_hire);
-        
+
     }
 }
