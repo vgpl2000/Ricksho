9e52b336139214fccadbeb7b7f835882e414410b
vgpl2000 <venugpl2000@gmail.com>
2023-04-16 15:12:55 +0530

    data adding to google auth, not adding to realtime

--- app/src/main/java/com/example/ricksho/user_login.java
+++ app/src/main/java/com/example/ricksho/user_login.java
@@ -23,6 +23,7 @@ import android.widget.Toast;
 import com.airbnb.lottie.LottieAnimationView;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
@@ -30,9 +31,14 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.SignInMethodQueryResult;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.regex.Pattern;
 
 public class user_login extends AppCompatActivity {
@@ -41,6 +47,7 @@ public class user_login extends AppCompatActivity {
     String txtemail,txtfname,txtlname,txtpass;
     FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
     DatabaseReference reference=firebaseDatabase.getReference("user");
+    private Timer timer;
 
     private FirebaseAuth mAuth;
 
@@ -105,10 +112,12 @@ public class user_login extends AppCompatActivity {
                     loadingAnimation.playAnimation();
                     //mail is available in proper format and now check user is created already or not
                     //signing in
-                    FirebaseUser currentUser=mAuth.getCurrentUser();
-                    if(currentUser!=null){
-                        //check user with that email id exists or not
-                        mAuth.fetchSignInMethodsForEmail(txtemail)
+
+                            if(mAuth.getCurrentUser()!=null)
+                            {
+                                //when current user already created in firebase realtime database then sign in...
+                                //check user with that email id exists or not
+                                mAuth.fetchSignInMethodsForEmail(txtemail)
                                         .addOnCompleteListener(user_login.this, new OnCompleteListener<SignInMethodQueryResult>() {
                                             @Override
                                             public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
@@ -150,7 +159,6 @@ public class user_login extends AppCompatActivity {
                                                                     }
                                                                 });
                                                     }
-
                                                 }else{
                                                     //if not give error message
                                                     // Hide the loading view
@@ -161,89 +169,117 @@ public class user_login extends AppCompatActivity {
                                                 }
                                             }
                                         });
-                    }else{
-                        //if there doesn't exist any user already with email, creating new user account
-                        // Hide the loading view
-                        loadingView.setVisibility(View.GONE);
-                        loadingAnimation.cancelAnimation();
+
+                            }else{
+                                //user doesn't already exists, then create new
+                                //if there doesn't exist any user already with email, creating new user account
+                                // Hide the loading view
+                                loadingView.setVisibility(View.GONE);
+                                loadingAnimation.cancelAnimation();
+
+                                //create user with mail and password
+                                textInputLayout.setVisibility(View.VISIBLE);
+                                editTextPassword.setVisibility(View.VISIBLE);
+                                txtpass=editTextPassword.getText().toString();
+                                if(txtpass.isEmpty()){
+                                    editTextPassword.setError("Password cannot be Empty!");
+                                }
+                                else {
+                                    loadingView.setVisibility(View.VISIBLE);
+                                    loadingAnimation.playAnimation();
+
+                                    mAuth.createUserWithEmailAndPassword(txtemail, txtpass)
+                                            .addOnCompleteListener(user_login.this, new OnCompleteListener<AuthResult>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                                    if (task.isSuccessful()) {
+                                                        FirebaseUser user = mAuth.getCurrentUser();
+                                                        Toast.makeText(user_login.this, "User created!", Toast.LENGTH_SHORT).show();
 
 
-                        f_name.setVisibility(View.VISIBLE);
-                        l_name.setVisibility(View.VISIBLE);
-                        editTextPassword.setVisibility(View.VISIBLE);
-                        textInputLayout.setVisibility(View.VISIBLE);
+                                                        //verify email
+                                                        //send verification email after verifying then proceed
+                                                        user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                if (task.isComplete()) {
 
-                        txtfname=f_name.getText().toString();
-                        txtlname=l_name.getText().toString();
-                        txtpass=editTextPassword.getText().toString();
-
-                        if(txtfname.isEmpty()){
-                            f_name.setError("First Name cannot be Empty!");
-                        }
-                        else if(txtlname.isEmpty()){
-                            l_name.setError("Last Name cannot be Empty!");
-                        }
-                        else if(txtpass.isEmpty()){
-                            editTextPassword.setError("Password cannot be Empty!");
-                        }
-                        else{
-                            //if all data are given then create user account
-                            // Show the loading view
-                            loadingView.setVisibility(View.VISIBLE);
-                            loadingAnimation.playAnimation();
-/*
-                            //Verify that email verified or not
-                            if (mAuth.getCurrentUser().isEmailVerified()) {
-                                //Email verified
-                                Toast.makeText(user_login.this, "User email verified", Toast.LENGTH_SHORT).show();
-
-                            }
-*/
-                            mAuth.createUserWithEmailAndPassword(txtemail,txtpass)
-                                    .addOnCompleteListener(user_login.this, new OnCompleteListener<AuthResult>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<AuthResult> task) {
-                                            if(task.isSuccessful()) {
-                                                //Show loading
-                                                loadingView.setVisibility(View.VISIBLE);
-                                                loadingAnimation.playAnimation();
-                                                //verify email by sending verification link to mail
-                                                mAuth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
-                                                    @Override
-                                                    public void onComplete(@NonNull Task<Void> task) {
-                                                        if (task.isSuccessful()) {
-                                                            String uid = mAuth.getCurrentUser().getUid();
-                                                            //Adding to realtime database
-                                                            DatabaseReference ref1 = reference.child(uid);
-                                                            ref1.child("fname").setValue(txtfname);
-                                                            ref1.child("lname").setValue(txtlname);
-                                                            ref1.child("email").setValue(txtemail);
-                                                            ref1.child("password").setValue(txtpass);
-                                                            FirebaseUser user = mAuth.getCurrentUser();
-                                                            Toast.makeText(user_login.this, "Verification link has been sent to your mail id!", Toast.LENGTH_LONG).show();
-                                                            // Hide the loading view
-                                                            loadingView.setVisibility(View.GONE);
-                                                            loadingAnimation.cancelAnimation();
-                                                        }
+                                                                    //mail is sent, when verified display next
+                                                                    //adding timer to check every 2 seconds that email is verified or not
+                                                                    timer = new Timer();
+                                                                    timer.schedule(new TimerTask() {
+                                                                        @Override
+                                                                        public void run() {
+                                                                            user.reload().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                                                @Override
+                                                                                public void onSuccess(Void unused) {
+                                                                                    if (user.isEmailVerified()) {
+                                                                                        //once user is verified, cancel the timer
+                                                                                        timer.cancel();
+                                                                                        // Hide the loading view
+                                                                                        loadingView.setVisibility(View.GONE);
+                                                                                        loadingAnimation.cancelAnimation();
+                                                                                        //mail verified then display next
+                                                                                        f_name.setVisibility(View.VISIBLE);
+                                                                                        l_name.setVisibility(View.VISIBLE);
+                                                                                        txtfname = f_name.getText().toString();
+                                                                                        txtlname = l_name.getText().toString();
 
 
+                                                                                        if (txtfname.isEmpty()) {
+                                                                                            f_name.setError("First Name cannot be Empty!");
+                                                                                        } else if (txtlname.isEmpty()) {
+                                                                                            l_name.setError("Last Name cannot be Empty!");
+                                                                                        } else {
+                                                                                            //if all data are given then create user account
+                                                                                            // Show the loading view
+                                                                                            loadingView.setVisibility(View.VISIBLE);
+                                                                                            loadingAnimation.playAnimation();
+
+                                                                                            //Adding to realtime database
+                                                                                            String uid = user.getUid();
+                                                                                            DatabaseReference ref1 = reference.child(uid);
+                                                                                            ref1.child("fname").setValue(txtfname);
+                                                                                            ref1.child("lname").setValue(txtlname);
+                                                                                            ref1.child("email").setValue(txtemail);
+                                                                                            ref1.child("password").setValue(txtpass);
+                                                                                        }
+
+                                                                                    } else {
+                                                                                        runOnUiThread(new Runnable() {
+                                                                                            @Override
+                                                                                            public void run() {
+                                                                                                Toast.makeText(user_login.this, "Verification link sent to your email id!", Toast.LENGTH_SHORT).show();
+                                                                                            }
+                                                                                        });
+                                                                                    }
+
+                                                                                }
+                                                                            });
+
+                                                                        }
+                                                                    }, 1, 3000);
+                                                                } else {
+                                                                    Toast.makeText(user_login.this, "Failed to send mail to " + txtemail, Toast.LENGTH_SHORT).show();
+                                                                }
+
+                                                            }
+                                                        });
+                                                    } else {
+                                                        loadingView.setVisibility(View.GONE);
+                                                        loadingAnimation.cancelAnimation();
+
+                                                        Toast.makeText(user_login.this, "Failed to register! Try Again", Toast.LENGTH_SHORT).show();
                                                     }
-                                                });
-                                            }else{
-                                                loadingView.setVisibility(View.GONE);
-                                                loadingAnimation.cancelAnimation();
+                                                }
+                                            });
 
-                                                Toast.makeText(user_login.this, "User is not created", Toast.LENGTH_SHORT).show();
-                                            }
-                                        }
-                                    });
-                        }
-
-                    }
+                                }
 
                 }
             }
-        });
-    }
+        };
+    });
 
+    }
 }
